a5610c78b328148948f5840df74b0fbb7b01fe4b
Astart01 <nekscalov@gmail.com>
2025-04-22 13:01:04 +0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def main():
             # format data for input format
             files = {"file": image.getvalue()}
             # send data and get the result
-            res = requests.post("http://127.0.0.1:8000/clf_image", files=files).json()
+            res = requests.post("http://158.160.70.219:8000/clf_image", files=files).json()
             # print results
             st.write(f'Class name: {res["class_name"]}, class index: {res["class_index"]}')
 
@@ -23,7 +23,7 @@ def main():
         txt = st.text_input('Classify text')
         if st.button('Classify'):
             text = {'text' : txt}
-            res = requests.post("http://127.0.0.1:8000/clf_text", json=text)
+            res = requests.post("http://158.160.70.219:8000/clf_text", json=text)
             st.write(res.json()['label'])
             st.write(res.json()['prob'])
         
@@ -39,7 +39,7 @@ def main():
                 # convert to input format
                 vector = {'feature1' : feature1, 'feature2' : feature2}
                 # send data and get the result
-                res = requests.post("http://127.0.0.1:8000/clf_table", json=vector)
+                res = requests.post("http://158.160.70.219:8000/clf_table", json=vector)
                 # print feature values and result
                 st.write("feature1", feature1, "feature2", feature2)
                 st.write(f"Predicted class is {res.json()['prediction']}")
